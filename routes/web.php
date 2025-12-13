d7cb87a228a7926181ac50595d3468e6115d5ad8
iMatheusPaula <imatheuspaula@gmail.com>
2025-12-13 18:51:51 -0300

    feat appointment routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AppointmentController::class)->group(function () {
+    Route::get('/appointments', 'index');
+    Route::post('/appointments', 'store');
+    Route::patch('/appointments/{appointment}/cancel', 'cancel');
 });
